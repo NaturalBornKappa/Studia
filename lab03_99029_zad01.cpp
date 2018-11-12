9e3eaf8af4e4ac4ed04c9ca11d1bb9aae2790cdd
Jakub Bauer <41141264+Quamian@users.noreply.github.com>
2018-11-12 12:06:48 +0100

    lab03_99029_zad01 doprowadzone do stanu kompilowalności; brak body funcji
    TO DO:
    - Add body of functions

--- lab03_99029_zad01.cpp
+++ lab03_99029_zad01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 using std::cin;
 using std::cout;
@@ -13,26 +14,26 @@ public:
 	Graph();
 	Graph(int n);
 
-	void add_vertex(Vertex* x = nullptr);
-	void add_edge(int n1, int n2);
-	void add_edge(Edge* n);
+	void add_vertex(Vertex* x = nullptr) {};
+	void add_edge(int n1, int n2) {};
+	void add_edge(Edge* n) {};
 
-	void set_to();
-	void set_from();
+	void set_to() {};
+	void set_from() {};
 
-	void print_edges();
+	void print_edges() {};
 
-	int get_vertices_number();
-	Vertex* get_vertex(int n);
+	int get_vertices_number() { return 0; };
+	Vertex* get_vertex(int n) { return 0; };
 
-	int get_edges_number();
+	int get_edges_number() { return 0; };
 
-	void remove_edge(int n);
-	void remove_vertex(int vertex_ideks);
+	void remove_edge(int n) {};
+	void remove_vertex(int vertex_ideks) {};
 
 private:
-	int vertex_indeks;  //index wiercho³ka
-	int vertex_number;  //iloœæ wierzcho³ków
+	int vertex_indeks;  //index wierzcholka
+	int vertex_number;  //ilosc wierzcholkow
 
 };
 
@@ -41,7 +42,7 @@ public:
 	Vertex();
 	Vertex(int n);
 
-	int set_weight(int n);
+	int set_weight(int n) { return 0; };
 	int vertex_number;
 
 private:
@@ -53,8 +54,8 @@ public:
 	Edge();
 	Edge(Vertex* n1, Vertex* n2);
 
-	void set_from(Vertex* n);
-	void set_to(Vertex* n);
+	void set_from(Vertex* n) {};
+	void set_to(Vertex* n) {};
 
 private:
 
@@ -88,17 +89,17 @@ Edge::Edge(Vertex* n1, Vertex* n2) {
 int main()
 {
 
-	Graph g(5); //pocz¹tkowa iloœæ wierzcho³ków 
+	Graph g(5); //poczatkowa ilosc wierzcholkow 
 	g.add_vertex();
-	Vertex* v = new Vertex(5); //waga wierzcho³ka
+	Vertex* v = new Vertex(5); //waga wierzcholka
 	g.add_vertex(v);
 	if (g.get_vertices_number() > 3)
 	{
 		g.get_vertex(0)->set_weight(2); //get_vertex przyjmuje
-										//za parametr indeks wierzcho³ka
-		g.add_edge(0, 1); //indeksy wierzcho³ków tworz¹cych krawêdŸ
+										//za parametr indeks wierzcholka
+		g.add_edge(0, 1); //indeksy wierzcholków tworzacych krawedz
 		g.add_edge(0, 3);
-		Vertex* v1 = g.get_vertex(1); //indeks wierzcho³ka
+		Vertex* v1 = g.get_vertex(1); //indeks wierzcholka
 		Vertex* v2 = g.get_vertex(2);
 		Edge* e = new Edge(v1, v2);
 		g.add_edge(e);
@@ -109,9 +110,9 @@ int main()
 	}
 	g.print_edges();
 	if (g.get_edges_number() > 1)
-		g.remove_edge(1); //indeks krawêdzi
+		g.remove_edge(1); //indeks krawedzi
 	if (g.get_vertices_number() > 1)
-		g.remove_vertex(1); //indeks wierzcho³ka
+		g.remove_vertex(1); //indeks wierzcholka
 
 
 	system("PAUSE");
