a0a7d5cd0542da9dd84de95b558dad69d2375e5c
Jakub Bauer <41141264+Quamian@users.noreply.github.com>
2019-05-15 15:17:24 +0200

    sortowania vol 3.1

--- AiSD/AiSD_sortowania/AiSD_sorotwania/selectionsortcpp.cpp
+++ AiSD/AiSD_sortowania/AiSD_sorotwania/selectionsortcpp.cpp
@@ -5,12 +5,11 @@ void selectionsort(int *_dane, int _size) {
 	int min, temporary;
 	for (int i = 0; i < _size; i++) {
 		min = i;
-		for (int j = i + 1; j < _size; j++) {
+		for (int j = i + 1; j <= _size; j++) {
 			if (_dane[j] < _dane[min])
 				min = j;
 		}
-		temporary = _dane[min];
-		_dane[min] = _dane[i];
-		_dane[i] = temporary;
+		swap_data(_dane[min], _dane[i]);
 	}
+	delete[] temp;
 }
