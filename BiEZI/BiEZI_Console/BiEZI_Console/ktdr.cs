d7f151cd877a0c30cf81e1834b3f61aec04fa834
Jakub Bauer <41141264+Quamian@users.noreply.github.com>
2019-12-15 14:59:44 +0100

    Coś tam z C#

---
+++ BiEZI/BiEZI_Console/BiEZI_Console/ktdr.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiEZI_ktdr
+{
+    class ktdr //który to dzień roku
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("WriteLine\n");
+            //Console.ReadLine();
+
+            int rok, mc, dz; // rok, miesiąc, dzień
+            int dzr=1; //dzień roku
+            Boolean prz = false; //rok przestępny
+
+            Console.Write("Podaj rok: ");
+            rok = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Podaj miesiąc: ");
+                mc = int.Parse(Console.ReadLine());
+            } while (mc < 0 || mc > 12);
+
+            do
+            {
+                Console.Write("Podaj dzień: ");
+                dz = int.Parse(Console.ReadLine());
+            } while (dz < 0);
+
+            if (rok % 4 == 0) prz = true;
+            if (rok % 100 == 0 && rok % 400 != 0) prz = false;
+            if ((mc > 2) && (prz == true)) dzr++;
+
+            switch (mc)
+            {
+                case 1:
+                    dzr = dz;
+                    break;
+                case 2:
+                    dzr = dz + 31;
+                    break;
+                case 3:
+                    dzr = dz + 31 + 28;
+                    break;
+                case 4:
+                    dzr = dz +31 + 28 + 31;
+                    break;
+                case 5:
+                    dzr = dz + 31 + 28 + 31 + 30;
+                    break;
+                case 6:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31;
+                    break;
+                case 7:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30;
+                    break;
+                case 8:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31;
+                    break;
+                case 9:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+                    break;
+                case 10:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+                    break;
+                case 11:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+                    break;
+                case 12:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+                    break;
+                default:
+                    Console.WriteLine("Nieprawidłowa wartość miesiąca, podany miesiąc nie istenienie w znanym mi kalendarzu.");
+                    break;
+            }
+
+            Console.WriteLine("Podana data to {0} dzień roku.", dzr);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BiEZI/WindowsFormsApplication1/WindowsFormsApplication1/UI.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class UI : Form
+    {
+        public UI()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rokBox_TextChanged(object sender, EventArgs e)
+        {
+            int x = int.Parse(rokBox.Text);
+        }
+
+        private void misiacBox_TextChanged(object sender, EventArgs e)
+        {
+            int y = int.Parse(miesiacBox.Text);
+        }
+
+        private void dzienBox_TextChanged(object sender, EventArgs e)
+        {
+            int z = int.Parse(dzienBox.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rok, mc, dz; // rok, miesiąc, dzień
+            int dzr = 1; //dzień roku
+            Boolean prz = false; //rok przestępny
+
+            rok = int.Parse(rokBox.Text);
+            mc = int.Parse(miesiacBox.Text);
+            dz = int.Parse(dzienBox.Text);
+
+            if (rok % 4 == 0) prz = true;
+            if (rok % 100 == 0 && rok % 400 != 0) prz = false;
+            if ((mc > 2) && (prz == true)) dzr++;
+
+            switch (mc)
+            {
+                case 1:
+                    dzr = dz;
+                    break;
+                case 2:
+                    dzr = dz + 31;
+                    break;
+                case 3:
+                    dzr = dz + 31 + 28;
+                    break;
+                case 4:
+                    dzr = dz + 31 + 28 + 31;
+                    break;
+                case 5:
+                    dzr = dz + 31 + 28 + 31 + 30;
+                    break;
+                case 6:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31;
+                    break;
+                case 7:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30;
+                    break;
+                case 8:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31;
+                    break;
+                case 9:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+                    break;
+                case 10:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+                    break;
+                case 11:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+                    break;
+                case 12:
+                    dzr = dz + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+                    break;
+                default:
+                    label4.Text = "Nieprawidłowy miesiąc!";
+                    break;
+            }
+
+            label4.Text = "Podana data to " + dzr + " dzień roku.";
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
